a2fa1d0f2ca6a43ea005e55d6544f67a67270b7f
KashishSharma13 <kashishsharma5877@gmail.com>
2025-03-03 14:38:48 +0530

    Initial commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './DescriptionBox.css'
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className='descriptionbox-navigation'>
+            <div className='descriptionbox-nav-box'>Description</div>
+            <div className='descriptionbox-nav-box fade'>Reviews(122)</div>
+        </div>
+    <div className='descriptionbox-description'>
+        <p> An e-commerce website is an online platform that facilitates buying and selling of products or services over the internet and serves as a virtual marketplace where businesses and individuals can showcase their products , interact with the customers, and conduct transactions without the need of physical presence. e-commerce website has gained an immense popularity due to their conventional accessibilities and the global reach they offer.  </p>
+        </div> 
+    </div>
+  )
+}
